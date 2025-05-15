69b6fc9edfb830c21ac8d3a3155a529d1952c371
Alexander Sholk <alexsholk@live.com>
2025-05-16 00:23:11 +0300

    Generalization

--- 2025_05_11_lcd/main/main.c
+++ 2025_05_11_lcd/main/main.c
@@ -1,8 +1,13 @@
 #include "lcd.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
     init_display();
+    clear_display();
+    draw_square_fractal(25000);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
     clear_display();
     draw_sierpinski(25000);
     invert_colors(true);
