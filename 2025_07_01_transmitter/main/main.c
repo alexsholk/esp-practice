080905ba9df5b755e297bd641ce42abe8448a2f6
Alexander Sholk <alexsholk@live.com>
2025-07-02 01:16:40 +0300

    Print MAC addresses loop

--- 2025_07_01_transmitter/main/main.c
+++ 2025_07_01_transmitter/main/main.c
@@ -3,37 +3,36 @@
 
 #include "esp_mac.h"
 
+void print_mac_addresses(void);
+
 void app_main(void)
 {
     printf("Transmitter\n");
+    print_mac_addresses();
+}
+
+void print_mac_addresses(void)
+{
+    static const struct
+    {
+        esp_mac_type_t type;
+        char* description;
+    } mac_types[] = {
+            {ESP_MAC_BASE, "Base MAC for that used for other MAC types"},
+            {ESP_MAC_WIFI_STA, "MAC for WiFi Station"},
+            {ESP_MAC_WIFI_SOFTAP, "MAC for WiFi Soft-AP"},
+            {ESP_MAC_BT, "MAC for Bluetooth"},
+            {ESP_MAC_ETH, "MAC for Ethernet"},
+            {ESP_MAC_EFUSE_FACTORY, "MAC_FACTORY eFuse which was burned by Espressif in production"},
+            {ESP_MAC_EFUSE_CUSTOM, "MAC_CUSTOM eFuse which was can be burned by customer"},
+        };
 
     uint8_t mac[6];
-
-    esp_read_mac(mac, ESP_MAC_BASE);
-    printf("Base MAC for that used for other MAC types: %X:%X:%X:%X:%X:%X\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-    memset(mac, 0, sizeof(mac));
-
-    esp_read_mac(mac, ESP_MAC_WIFI_STA);
-    printf("MAC for WiFi Station: %X:%X:%X:%X:%X:%X\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-    memset(mac, 0, sizeof(mac));
-
-    esp_read_mac(mac, ESP_MAC_WIFI_SOFTAP);
-    printf("MAC for WiFi Soft-AP: %X:%X:%X:%X:%X:%X\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-    memset(mac, 0, sizeof(mac));
-
-    esp_read_mac(mac, ESP_MAC_BT);
-    printf("MAC for Bluetooth: %X:%X:%X:%X:%X:%X\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-    memset(mac, 0, sizeof(mac));
-
-    esp_read_mac(mac, ESP_MAC_ETH);
-    printf("MAC for Ethernet: %X:%X:%X:%X:%X:%X\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-    memset(mac, 0, sizeof(mac));
-
-    esp_read_mac(mac, ESP_MAC_EFUSE_FACTORY);
-    printf("MAC_FACTORY eFuse which was burned by Espressif in production: %X:%X:%X:%X:%X:%X\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-    memset(mac, 0, sizeof(mac));
-
-    esp_read_mac(mac, ESP_MAC_EFUSE_CUSTOM);
-    printf("MAC_CUSTOM eFuse which was can be burned by customer: %X:%X:%X:%X:%X:%X\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-    memset(mac, 0, sizeof(mac));
+    for (int i = 0; i < sizeof(mac_types) / sizeof(mac_types[0]); i++)
+    {
+        if (ESP_OK == esp_read_mac(mac, mac_types[i].type))
+        {
+            printf("%s: " MACSTR "\n", mac_types[i].description, MAC2STR(mac));
+        }
+    }
 }
